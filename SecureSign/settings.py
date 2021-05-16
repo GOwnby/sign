91822dd09b49cd4786f11a34eb529b8cf6c0f500
Gracen Ownby <gracenownby@gmail.com>
2021-05-16 17:01:55 -0500

    debugging

--- SecureSign/settings.py
+++ SecureSign/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'IrisDocuments',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'Iris',
+        'PASSWORD': 'IrisPass',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
